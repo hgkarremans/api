4fe11fd19a401233d95de332497b4764f16f75d2
hgkarremans <hg.karremans@student.avans.nl>
2023-06-30 17:12:45 +0200

    testing

--- src/util/mysql-db.js
+++ src/util/mysql-db.js
@@ -1,20 +1,23 @@
-// get the client
-const mysql = require('mysql2');
-require('dotenv').config();
-
-// Create the connection pool. The pool-specific settings are the defaults
+const mysql = require('mysql2')
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  database: process.env.DB_NAME || 'shareameal',
-  port: process.env.DB_PORT || 3306,
-  password: process.env.DB_PASSWORD || '',
-  waitForConnections: true,
-  connectionLimit: 10,
-  multipleStatements: true,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0
-});
+    multipleStatements: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD  || '',
+    database: process.env.DB_DATABASE ||  'shareameal',
+})
 
-module.exports = pool;
+module.exports = pool
+
+pool.on('acquire', function (connection) {
+    console.log('Connection %d acquired', connection.threadId)
+})
+
+pool.on('release', function (connection) {
+    console.log('Connection %d released', connection.threadId)
+})
