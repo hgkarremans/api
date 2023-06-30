ffb5cf60dc12ac3de66590f88ce9c546aba53b7c
hgkarremans <hg.karremans@student.avans.nl>
2023-06-30 16:19:15 +0200

    changed database

--- src/util/mysql-db.js
+++ src/util/mysql-db.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  database: process.env.DB_NAME || 'shareameal',
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD || 'root',
   waitForConnections: true,
   connectionLimit: 10,
   multipleStatements: true,
