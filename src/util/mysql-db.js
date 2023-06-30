e860008c28a9e848c5b4b2daa5c1f19894513df0
hgkarremans <hg.karremans@student.avans.nl>
2023-06-30 17:31:34 +0200

    testing

--- src/util/mysql-db.js
+++ src/util/mysql-db.js
@@ -1,23 +1,33 @@
-const mysql = require('mysql2')
-const pool = mysql.createPool({
-    multipleStatements: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    queueLimit: 0,
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD  || '',
-    database: process.env.DB_DATABASE ||  'shareameal',
-})
+const mysql = require('mysql2');
+const logger = require('../util/utils').logger;
 
-module.exports = pool
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  database: process.env.DB_DATABASE || 'shareameal',
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD || '',
+  multipleStatements: true,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0
+});
+
+pool.on('connection', function (connection) {
+  logger.info(
+    `Connected to db '${connection.config.database}' on ${connection.config.host}`
+  );
+});
 
 pool.on('acquire', function (connection) {
-    console.log('Connection %d acquired', connection.threadId)
-})
+  logger.trace('Connection %d acquired', connection.threadId);
+});
 
 pool.on('release', function (connection) {
-    console.log('Connection %d released', connection.threadId)
-})
+  logger.trace('Connection %d released', connection.threadId);
+});
+
+module.exports = pool;
