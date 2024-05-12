97a8a6e3cb8fff2ef3ae1d31ba7eb3db4ea8efa0
hgkarremans <hg.karremans@student.avans.nl>
2024-05-12 22:28:17 +0200

    fixed loging in

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -5,7 +5,6 @@ var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const bcrypt = require('bcrypt');
 
-
 const apiController = {
 
   //UC-101
@@ -62,7 +61,7 @@ const apiController = {
   
             if (passwordMatch) {
               const userId = user.id;
-              const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+              const token = jwt.sign({ userId }, "your-secret-key");
               return res.status(200).json({
                 statusCode: 200,
                 message: "User login endpoint",
@@ -86,93 +85,6 @@ const apiController = {
       });
       conn.release();
     });
-  
-
-
-
-
-    // const { emailAdress, password } = req.body;
-    // const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-
-    // console.log("emailAdress: ", emailAdress);
-    // try {
-    //   assert(emailAdress != null, "emailAdress must be provided in request");
-    //   assert(password != null, "password must be provided in request");
-    //   assert(typeof emailAdress === "string", "emailAdress must be a string");
-    //   assert.match(emailAdress, emailRegex, "Email adress must be valid");
-    //   assert(typeof password === "string", "password must be a string");
-    // } catch (err) {
-    //   res.status(400).json({
-    //     status: 400,
-    //     message: err.message.toString(),
-    //   });
-    //   return;
-    // }
-
-    // let sqlStatement = "SELECT * FROM user WHERE emailAdress = ?";
-
-    // pool.getConnection(function (err, conn) {
-    //   if (err) {
-    //     console.log("error", err);
-    //     next("error: " + err.message);
-    //     return;
-    //   }
-
-    //   if (conn) {
-    //     conn.query(sqlStatement, [emailAdress], function (err, results, fields) {
-    //       if (err) {
-    //         console.log("error", err);
-    //         res.status(409).json({
-    //           code: 409,
-    //           message: err.message,
-    //         });
-    //         return;
-    //       }
-
-    //       if (results.length === 1) {
-    //         let user = results[0];
-    //         let storedPassword = user.password;
-
-    //         // Compare the provided password with the stored hashed password
-    //         bcrypt.compare(password, storedPassword, function (err, passwordMatch) {
-    //           if (err) {
-    //             console.log(err);
-    //             res.status(500).json({
-    //               statusCode: 500,
-    //               message: "Internal Server Error",
-    //             });
-    //             return;
-    //           }
-
-    //           if (passwordMatch) {
-    //             let userId = user.id;
-    //             const token = jwt.sign({ userId }, "your-secret-key");
-
-    //             res.status(200).json({
-    //               statusCode: 200,
-    //               message: "User login endpoint",
-    //               data: token,
-    //             });
-    //           } else {
-    //             res.status(401).json({
-    //               statusCode: 401,
-    //               message: "User login endpoint",
-    //               data: "Invalid credentials",
-    //             });
-    //           }
-    //         });
-    //       } else {
-    //         res.status(401).json({
-    //           statusCode: 401,
-    //           message: "User login endpoint",
-    //           data: "Invalid credentials",
-    //         });
-    //       }
-    //     });
-
-    //     pool.releaseConnection(conn);
-    //   }
-    // });
   },
 
 
